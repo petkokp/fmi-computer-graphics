e02416eeb093f0c71296a0ce0914edce0573c7c4
Petko Petkov <petko.petkov987@gmail.com>
2023-11-20 09:56:16 +0200

    flood fill recursive

--- homeworks/fill_circles_difference/fill_circles_difference.py
+++ homeworks/fill_circles_difference/fill_circles_difference.py
@@ -7,32 +7,58 @@ def draw_circle(ax, center, radius, color):
     circle = Circle(center, radius, edgecolor=color, facecolor='none')
     ax.add_patch(circle)
 
+
+def flood_recursive(matrix, start_x, start_y):
+    width = len(matrix)
+    height = len(matrix[0])
+
+    def fill(x, y, start_color, color_to_update):
+        if matrix[x][y] != start_color:
+            return
+
+        elif matrix[x][y] == color_to_update:
+            return
+        else:
+            matrix[x][y] = color_to_update
+            neighbors = [(x-1, y), (x+1, y), (x-1, y-1), (x+1, y+1),
+                         (x-1, y+1), (x+1, y-1), (x, y-1), (x, y+1)]
+            for n in neighbors:
+                if 0 <= n[0] <= width-1 and 0 <= n[1] <= height-1:
+                    fill(n[0], n[1], start_color, color_to_update)
+
+    start_color = matrix[start_x][start_y]
+    fill(start_x, start_y, start_color, 9)
+    return matrix
+
+
 def fill_recursive(ax, polygon, fill_color, alpha):
     if polygon.geom_type == 'Polygon':
         x, y = polygon.exterior.xy
         ax.fill(x, y, fill_color, alpha=alpha)
         plt.draw()
         time.sleep(0.5)
-       
+
     elif polygon.geom_type == 'MultiPolygon':
         for part in polygon:
             if part.geom_type == 'Polygon':
                 fill_recursive(ax, part, fill_color, alpha)
-            
+
+
 def fill_circle_symmetric_difference(ax, center1, radius1, center2, radius2, fill_color):
     circle1 = Point(center1).buffer(radius1)
     circle2 = Point(center2).buffer(radius2)
 
     symmetric_difference_polygon = circle1.symmetric_difference(circle2)
-    
+
     draw_circle(ax, center1, radius1, color='blue')
 
     draw_circle(ax, center2, radius2, color='orange')
-    
-    fill_recursive(ax, symmetric_difference_polygon, fill_color='green', alpha=0.5)
+
+    fill_recursive(ax, symmetric_difference_polygon,
+                   fill_color='green', alpha=0.5)
 
     plt.show()
-    
+
     try:
         print("Click on a point to check if it's in the symmetric difference area:")
         point_x, point_y = plt.ginput(1)[0]
@@ -41,20 +67,24 @@ def fill_circle_symmetric_difference(ax, center1, radius1, center2, radius2, fil
 
         # Change point validation code to verify whether the Point is within circle1 and circle2 or not.
         if point.within(symmetric_difference_polygon):
-            print(f"The point ({point_x}, {point_y}) is within the symmetric difference area.")
+            print(
+                f"The point ({point_x}, {point_y}) is within the symmetric difference area.")
             print("Filling the symmetric difference area gradually in red...")
 
             # Gradually fill the symmetric difference area in red using recursion
-            fill_recursive(ax, symmetric_difference_polygon, fill_color='red', alpha=1)
+            fill_recursive(ax, symmetric_difference_polygon,
+                           fill_color='red', alpha=1)
 
             print("Filling completed.")
             plt.show()
         else:
-            print(f"The point ({point_x}, {point_y}) is not within the symmetric difference area.")
+            print(
+                f"The point ({point_x}, {point_y}) is not within the symmetric difference area.")
     except ValueError:
         print("Невалиден вход.")
     except KeyboardInterrupt:
         print("\nПроцесът е прекъснат.")
+
 
 def fill_circle_difference(ax, center1, radius1, center2, radius2, fill_color):
     # Create Shapely circles
@@ -94,11 +124,13 @@ def fill_circle_difference(ax, center1, radius1, center2, radius2, fill_color):
             print("Запълнено.")
             plt.show()
         else:
-            print(f"Точката ({point_x}, {point_y}) не е в областта на разликата..")
+            print(
+                f"Точката ({point_x}, {point_y}) не е в областта на разликата.")
     except ValueError:
         print("Невалиден вход.")
     except KeyboardInterrupt:
         print("\nПроцесът е прекъснат.")
+
 
 # Set up the figure and axis
 fig, ax = plt.subplots(figsize=(8, 8))
@@ -112,6 +144,7 @@ radius2 = 0
 # Set constant x and y axis limits
 ax.set_xlim(0, 10)
 ax.set_ylim(0, 10)
+
 
 def onclick(event):
     global center1, center2, radius1, radius2
@@ -131,7 +164,9 @@ def onclick(event):
         draw_circle(ax, center2, radius2, color='orange')
 
         # fill_circle_symmetric_difference(ax, center1, radius1, center2, radius2, fill_color='green')
-        fill_circle_difference(ax, center1, radius1, center2, radius2, fill_color='green')
+        fill_circle_difference(ax, center1, radius1,
+                               center2, radius2, fill_color='green')
+
 
 # Connect the event handler
 fig.canvas.mpl_connect('button_press_event', onclick)
